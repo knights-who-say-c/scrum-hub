672f88d65dab8f8f81782bd53965c73222a7b92e
Armani Merlino <35540163+a-merlino@users.noreply.github.com>
2021-09-21 12:47:42 -0400

    Created basic login/sign up pages
    Pages load and each has a form for submitting some information. Log in needs more fields and actual functionality.
    
    Layout needs tweaks (oversized logo)

--- login/login.py
+++ login/login.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/login')
 def login():
-    return send_file("static/login.html")
+    return render_template("login.html", title = "Log In")
+
+@app.route('/register')
+def register():
+    return render_template("login.html", title = "Sign Up")
 
 app.run(host='0.0.0.0', port=8000)

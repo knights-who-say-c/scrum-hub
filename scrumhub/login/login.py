22fea8bfd2cd588957ece7546cc7349318424b87
rwwilkow <rwwilkow@buffalo.edu>
2021-09-30 21:41:01 -0400

    #40 Basic HTML for home page

--- scrumhub/login/login.py
+++ scrumhub/login/login.py
@@ -2,12 +2,16 @@ from flask import *
 
 app = Flask(__name__)
 
+@app.route('/home')
+def register():
+    return render_template("home.html", title = "Home Page")
+
 @app.route('/login')
 def login():
     return render_template("login.html", title = "Log In")
 
 @app.route('/register')
-def register():
+def home():
     return render_template("login.html", title = "Sign Up")
 
 # app.run(host='0.0.0.0', port=8000)

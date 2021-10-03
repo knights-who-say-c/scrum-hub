62212a44074eac9b6ad1c4b28bf584173a3e3fc8
Armani Merlino <35540163+a-merlino@users.noreply.github.com>
2021-10-02 20:30:17 -0400

    Theoretically basic functionality
    html now sends form to login.py.
    
    Registration page makes basic checks to determine email and password validity.
    
    Currently not connecting to database, can't find a straightforward tutorial on psycopg2 + heroku that uses DATABASE_URL.

--- login/login.py
+++ login/login.py
@@ -1,6 +1,20 @@
 from flask import *
 
+import os
+import psycopg2
+
 app = Flask(__name__)
+
+def validEmail(email):
+    at = email.find("@")
+    dot = email.find(".", start = at)
+    return at != -1 and dot != -1
+
+def validPassword(password, confirm):
+    length = len(password) > 6
+    confirmation = password == confirm
+    
+    return length and confirmation
 
 @app.route('/login')
 def login():
@@ -10,4 +24,46 @@ def login():
 def register():
     return render_template("login.html", title = "Sign Up")
 
+@app.route('/loginRequest', methods = ['POST'])
+def handleLogin():
+    if request.method == 'POST':
+        formData = request.form
+
+        email = formData['email']
+        password = formData['password']
+
+    return render_template("login.html", title = "Log In")
+
+
+@app.route('/registerRequest', methods = ['POST'])
+def handleRegister():
+    if request.method == 'POST':
+        formData = request.form
+
+        name = (formData['firstName'], formData['lastName'])
+
+        email = formData['email']
+        password = formData['password']
+        passwordConfirm = formData['passwordConfirm']
+
+        feedback = ""
+        if not validPassword(password, passwordConfirm):
+            feedback += "Password and confirmation must be the same <br/>"
+
+        if not validEmail(email):
+            feedback += "Email is not a valid address <br/>"
+        
+    return render_template("login.html", title = "Sign Up", feedback = feedback)
+
+
+# DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+db_config = os.environ['DATABASE_URL'] if 'DATABASE_URL' in os.environ else 'user=postgres password=password'
+
+database = psycopg2.connect(db_config, sslmode='require')
+
+
+print("Does this work?")
+
 app.run(host='0.0.0.0', port=8000)
+
+

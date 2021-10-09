1cb5843e9aa734ee6a3209de98aa8bc8c9689384
Armani Merlino <35540163+a-merlino@users.noreply.github.com>
2021-10-09 16:19:03 -0400

    Change database connection to use DATABASE_URL parameter, disallow multiple accounts for a given email #43
    Now uses Heroku's DATABASE_URL config var. Functionally the same as before, but won't have to be manually updated.
    
    handleRegister() now checks for if an account with that email exists. If so, blocks creation of the duplicate account.

--- login/login.py
+++ login/login.py
@@ -5,20 +5,10 @@ import psycopg2
 
 app = Flask(__name__)
 
-DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
 DATABASE_URL = os.environ['DATABASE_URL']
-
-''' These credentials are bound to change over time.
-    Can't figure out Heroku's DATABASE_URL so this has to do.... '''
-
-HOST = "ec2-34-233-187-36.compute-1.amazonaws.com"
-DATABASE = "dbdb4ogu09rgqg"
-USER = "wrwgrvzkfihtdb"
-PORT = "5432"
-
-#database = psycopg2.connect(DATABASE_URL, password=DATABASE_PASSWORD, sslmode='require')
-database = psycopg2.connect(dbname = DATABASE, user = USER, password = DATABASE_PASSWORD, host = HOST, port = PORT)
+database = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = database.cursor()
+database.autocommit = True
 
 cur.execute("CREATE TABLE IF NOT EXISTS testLogins (firstName text, lastName text, email text, password text)")
 
@@ -68,6 +58,12 @@ def handleRegister():
 
         if not validEmail(email):
             msg += "Email is not a valid address <br/>"
+
+        cur.execute("SELECT email FROM testLogins WHERE email = %s", (email,))
+        existing = cur.fetchone()
+
+        if existing:
+            msg = "Account already exists for " + email
 
         if not msg:
             cur.execute("INSERT INTO testLogins (firstName, lastName, email, password) VALUES(%s, %s, %s, %s)", (first, last, email, password))

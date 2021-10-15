e91e8f00c14fd4c6c8030d457a7a2f272e95f818
Armani Merlino <35540163+a-merlino@users.noreply.github.com>
2021-10-14 21:56:05 -0400

    Add upload button in HTML #51
    Needs JS to show/hide button, as well as actual functionality.

---
+++ scrumhub/login/static/project.js
@@ -0,0 +1,9 @@
+<script language="JavaScript" type="text/javascript">
+
+function fileUpload(display){
+if(display == "no"){
+    document.getElementById('popupbox').style.visibility="hidden";}
+else if(display == "yes"){
+    document.getElementById('popupbox').style.visibility="visible";}
+}
+</script>

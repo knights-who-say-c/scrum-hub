8345bb1a95f18abd9774a9447e9c643be8b4108b
xwiss <ericvelepucha@gmail.com>
2021-10-18 03:35:32 -0400

    #53 profile page can edit user info in db

--- scrumhub/login/login.py
+++ scrumhub/login/login.py
@@ -1,6 +1,33 @@
 from flask import *
+import os
+import psycopg2
 
 app = Flask(__name__)
+app.secret_key = "testkey1"
+
+DATABASE_URL = os.environ['DATABASE_URL']
+database = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = database.cursor()
+database.autocommit = True
+
+cur.execute("CREATE TABLE IF NOT EXISTS testLogins (firstName text, lastName text, email text, password text)")
+
+# session == user currently logged in's information
+session['email'] = ""
+
+def validEmail(email):
+    at = email.find("@")
+    dot = email.find(".")
+    return at != -1 and dot > at
+
+def validPassword(password, confirm):
+    length = len(password) > 6
+    confirmation = password == confirm
+    
+    return length and confirmation
+
+def authenticate(email, password):
+   return False
 
 @app.route('/login')
 def login():
@@ -14,6 +41,35 @@ def register():
 def profile():
     return render_template("profile.html", title = "Profile")
 
-# app.run(host='0.0.0.0', port=8000)
-# line 13 not necessary to run in container
-# according to docker documentation
+@app.route('/profileUpdater', methods = ['POST'])
+def handleUpdate():
+    if request.method == 'POST':
+        formData = request.form
+        
+        first = formData['fname']
+        last = formData['lname']
+        email = formData['email']
+        password = formData['password']
+        passwordConfirm = formData['cpass']
+
+        msg = ""
+
+        if first: cur.execute("UPDATE testLogins SET firstName = (%s) WHERE email = (%s)",(first, session['email'],))
+        if last: cur.execute("UPDATE testLogins SET lastName = (%s) WHERE email = (%s)",(last, session['email'],))
+
+        if password:
+            if not validPassword(password, passwordConfirm): msg += "Password and confirmation must be the same <br/>"
+            else: cur.execute("UPDATE testLogins SET password = (%s) WHERE email = (%s)",(password, session['email'],))
+        
+        if email:
+            if not validEmail(email): msg += "Email is not a valid address <br/>"
+            else:
+                cur.execute("UPDATE testLogins SET email = (%s) WHERE email = (%s)",(email, session['email'],))
+                session['email'] = email
+
+        if not msg:
+            flash("Saved Changes!")
+        else:
+            flash(msg)
+    
+    return redirect("profile", code=301)
